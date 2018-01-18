c97424dd11702ce7b7f0eb676587f4da31e1c8f9
kylegibson15 <kylegibson15@gmail.com>
2018-01-17 22:19:45 -0700

    navigation component for easy top <--> bottom scrolling; working

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,8 +5,8 @@ export default class Navigation extends Component {
   render(){
     return (
       <div className="navigation-container" style={{ position: this.props.scrollingLock ? "fixed" : "absolute", top: this.props.scrollingLock ? "0" : null}}>
-        <a onClick={this.props.goToContactPage}className="contact-info">
-          <h5>Contact Me</h5>
+        <a onClick={this.props.goToContactPage} className="contact-info">
+          <h5>{this.props.contactOrTop ? "Contact Me" : "Back To Top"}</h5>
         </a>
       </div>
     )
