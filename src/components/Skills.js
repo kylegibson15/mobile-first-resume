896737c10bbcd7cdd889b291bf5510564519f4e7
kylegibson15 <kylegibson15@gmail.com>
2018-01-19 16:49:25 -0700

    fixed lang logos and tech

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,17 +1,18 @@
 import React, {Component} from 'react';
-import javascript from '../assets/images/JavaScript-logo.png';
-import python from '../assets/images/Logo_Python.png';
+import javascript from '../assets/images/js-no-border.svg';
+import python from '../assets/images/python-no-border.svg';
 import firebase from '../assets/images/android_firebase.png';
 import bootstrap from '../assets/images/bootstrap_solid.png';
 import express from '../assets/images/Expressjs.png';
 import git from '../assets/images/git.png';
 import handlebars from '../assets/images/handlebars2.png';
 import heroku from '../assets/images/herokukafka.png'
-import html_css from '../assets/images/html_css.png';
+import html from '../assets/images/html5-no-border.svg';
+import css from '../assets/images/css3-no-border.svg';
 import knex from '../assets/images/knexJS.png';
 import jquery from '../assets/images/logo-jquery-png--800.gif';
 import redux from '../assets/images/logo-redux.png';
-import react from '../assets/images/reactjs.png';
+import react from '../assets/images/react-no-border.png';
 import mocha_chai from '../assets/images/mocha-chai.png';
 import node from '../assets/images/nodejs-128.png';
 import sql from '../assets/images/sql.png';
@@ -37,23 +38,24 @@ export default class Skills extends Component {
         <div className="languages">LANGUAGES</div>
 
         <div className="logos-under">
-          <img className="lang-logo" alt="react" src={react}/>
-          <img className="lang-logo" alt="redux" src={redux}/>
-          <img className="lang-logo" alt="javascript" src={javascript}/>
-          <img className="lang-logo" alt="python" src={python}/>
-          <img className="lang-logo" alt="handlebars" src={handlebars}/>
-          <img className="lang-logo" alt="html css" src={html_css}/>
-          <img className="lang-logo" alt="webpack" src={webpack}/>
-          <img className="lang-logo" alt="heroku" src={heroku}/>
-          <img className="lang-logo" alt="git" src={git}/>
-          <img className="lang-logo" alt="firebase" src={firebase}/>
-          <img className="lang-logo" alt="express" src={express}/>
-          <img className="lang-logo" alt="node" src={node}/>
-          <img className="lang-logo" alt="sql" src={sql}/>
-          <img className="lang-logo" alt="bootstrap" src={bootstrap}/>
-          <img className="lang-logo" alt="jquery" src={jquery}/>
-          <img className="lang-logo" alt="mocha_chai" src={mocha_chai}/>
-          <img className="lang-logo knex" alt="knex" src={knex}/>
+          <img className="lang-logo react" alt="react" src={react}/>
+          <img className="lang-logo redux" alt="redux" src={redux}/>
+          <img className="lang-logo js" alt="javascript" src={javascript}/>
+          <img className="lang-logo python" alt="python" src={python}/>
+          {/* <img className="lang-logo" alt="handlebars" src={handlebars}/> */}
+          <img className="lang-logo html" alt="html css" src={html}/>
+          {/* <img className="lang-logo" alt="html css" src={css}/> */}
+          {/* <img className="lang-logo" alt="webpack" src={webpack}/> */}
+          {/* <img className="lang-logo" alt="heroku" src={heroku}/> */}
+          <img className="lang-logo git" alt="git" src={git}/>
+          {/* <img className="lang-logo" alt="firebase" src={firebase}/> */}
+          {/* <img className="lang-logo" alt="express" src={express}/> */}
+          {/* <img className="lang-logo" alt="node" src={node}/> */}
+          {/* <img className="lang-logo" alt="sql" src={sql}/> */}
+          {/* <img className="lang-logo" alt="bootstrap" src={bootstrap}/> */}
+          {/* <img className="lang-logo" alt="jquery" src={jquery}/> */}
+          {/* <img className="lang-logo" alt="mocha_chai" src={mocha_chai}/> */}
+          {/* <img className="lang-logo knex" alt="knex" src={knex}/> */}
         </div>
       </div>
 
@@ -62,11 +64,11 @@ export default class Skills extends Component {
         <div className="languages">TOOLS</div>
 
         <div className="logos-under tools-under">
-          <img className="lang-logo" alt="slack" src={slack}/>
-          <img className="lang-logo" alt="github" src={github}/>
-          <img className="lang-logo" alt="chrome" src={chrome}/>
-          <img className="lang-logo" alt="raspberry pi" src={raspberry_pi}/>
-          <img className="lang-logo" alt="pivotal tracker" src={pivotal}/>
+          <img className="lang-logo chrome" alt="chrome" src={chrome}/>
+          <img className="lang-logo slack" alt="slack" src={slack}/>
+          <img className="lang-logo github" alt="github" src={github}/>
+          <img className="lang-logo rp" alt="raspberry pi" src={raspberry_pi}/>
+          <img className="lang-logo pivotal" alt="pivotal tracker" src={pivotal}/>
         </div>
       </div>
 
@@ -75,10 +77,10 @@ export default class Skills extends Component {
         <div className="languages">TECHNIQUES</div>
 
         <div className="logos-under tools-under techniques">
-          <img className="lang-logo" alt="responsive design" src={responsive_design}/>
-          <img className="lang-logo" alt="pair programming" src={pair_programmming}/>
-          <img className="lang-logo" alt="agile" src={agile}/>
-          <img className="lang-logo" alt="tdd" src={tdd}/>
+          <img className="lang-logo pair" alt="pair programming" src={pair_programmming}/>
+          <img className="lang-logo rd" alt="responsive design" src={responsive_design}/>
+          <img className="lang-logo agile" alt="agile" src={agile}/>
+          <img className="lang-logo tdd" alt="tdd" src={tdd}/>
         </div>
       </div>
 
