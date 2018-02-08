fa59bb9dd6d97d0ed3b0acf3d1ee7fa000f1f76a
kylegibson15 <kylegibson15@gmail.com>
2018-02-07 19:00:25 -0700

    change color of text when nav button gets to the bottom so that it is visible

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,7 +5,7 @@ export default class Navigation extends Component {
   render(){
     return (
       <div className="navigation-container" style={{ position: this.props.scrollingLock ? "fixed" : "absolute", top: this.props.scrollingLock ? "0" : null}}>
-        <a onClick={this.props.goToContactPage} className="contact-info">
+        <a onClick={this.props.goToContactPage} className="contact-info" style={{color: this.props.contactOrTop ? "black" : "white", borderBottom: this.props.contactOrTop ? null : ".2px solid white" }}>
           <h5>{this.props.contactOrTop ? "Contact Me" : "Back To Top"}</h5>
         </a>
       </div>
