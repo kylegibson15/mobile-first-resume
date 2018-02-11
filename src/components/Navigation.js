85b408b811921750d6974a290281fe8364c54604
kylegibson15 <kylegibson15@gmail.com>
2018-02-10 23:39:09 -0700

    update nav capabilities

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import logo from '../assets/images/main/blue.png';
 
 export default class Navigation extends Component {
 
   render(){
     return (
       <div className="navigation-container" style={{ position: this.props.scrollingLock ? "fixed" : "absolute", top: this.props.scrollingLock ? "0" : null}}>
-        <a onClick={this.props.goToContactPage} className="contact-info" style={{color: this.props.contactOrTop ? "black" : "white", borderBottom: this.props.contactOrTop ? null : ".2px solid white" }}>
+        <img className="nav-logo" src={logo}/>
+        <a onClick={this.props.goToContactPage} className="contact-info" >
           <h5>{this.props.contactOrTop ? "Contact Me" : "Back To Top"}</h5>
         </a>
       </div>
