03cca9ac0a0a494e80e0ecaaa2d5c6123d48fab7
kylegibson15 <kylegibson15@gmail.com>
2018-02-13 22:46:13 -0700

    remove some lines, throw some AWS links in there

--- src/components/ContactV2.js
+++ src/components/ContactV2.js
@@ -1,10 +1,4 @@
 import React, {Component} from 'react';
-import github from '../assets/images/white-github.png';
-import linkedin from '../assets/images/white-linkedin-icon.png';
-import facebook from '../assets/images/white-fb-icon-facebook.png';
-import instagram from '../assets/images/white-instagram-icon.png';
-import phone from '../assets/images/white-phone.png';
-import email from '../assets/images/white-G-mail.png';
 
 export default class ContactV2 extends Component {
 
@@ -17,32 +11,20 @@ export default class ContactV2 extends Component {
 
       <div className="infov2">
         <div className="phone-emailv2">
-          <a href="tel:+12705773957" className="phone"><img className="ph-mail-icon" alt="orange-phone" src={phone}/>CALL</a>
-          <a className="email" href="mailto:kylegibson15@gmail.com"><img className="ph-mail-icon" alt="orange-email" src={email}/>MAIL</a>
+          <a href="tel:+12705773957" className="phone"><img className="ph-mail-icon" alt="orange-phone" src='https://s3.amazonaws.com/www.kylegibson15.com/white-phone.png'/>CALL</a>
+          <a className="email" href="mailto:kylegibson15@gmail.com"><img className="ph-mail-icon" alt="orange-email" src='https://s3.amazonaws.com/www.kylegibson15.com/white-G-mail.png'/>MAIL</a>
         </div>
 
-
-        {/* <div className="sagan-quote">
-          <h6>Somewhere, something incredible is waiting to be known.</h6>
-          <h5>- Carl Sagan</h5>
-        </div> */
-        }
         <div className="social-links">
-          <a href="https://www.facebook.com/kyle.gibson.3194" target="_blank"><img className="contact-icon" alt="facebook" src={facebook}/></a>
+          <a href="https://www.facebook.com/kyle.gibson.3194" target="_blank"><img className="contact-icon" alt="facebook" src='https://s3.amazonaws.com/www.kylegibson15.com/white-fb-icon-facebook.png'/></a>
 
-          <a href="https://github.com/kylegibson15" target="_blank"><img className="contact-icon" alt="github" src={github}/></a>
+          <a href="https://github.com/kylegibson15" target="_blank"><img className="contact-icon" alt="github" src='https://s3.amazonaws.com/www.kylegibson15.com/white-github.png'/></a>
 
-          <a href="https://www.linkedin.com/in/kylegibson15" target="_blank"><img className="contact-icon" alt="linkedIn" src={linkedin}/></a>
+          <a href="https://www.linkedin.com/in/kylegibson15" target="_blank"><img className="contact-icon" alt="linkedIn" src='https://s3.amazonaws.com/www.kylegibson15.com/white-linkedin-icon.png'/></a>
 
-          <a href="https://www.instagram.com/1gibby4/?hl=en" target="_blank"><img className="contact-icon" alt="instagram" src={instagram}/></a>
+          <a href="https://www.instagram.com/1gibby4/?hl=en" target="_blank"><img className="contact-icon" alt="instagram" src='https://s3.amazonaws.com/www.kylegibson15.com/white-instagram-icon.png'/></a>
         </div>
       </div>
-
-
-      {/* <div className="copyright">
-        <p>&copy; Kyle Gibson</p>
-      </div> */
-      }
 
     </div>)
   }
