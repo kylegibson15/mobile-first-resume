46b6e7986c80f6e1427d0295113067436eacb5d8
kylegibson15 <kylegibson15@gmail.com>
2018-02-13 20:44:52 -0700

    changed the nav button to lowercase text

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -8,7 +8,7 @@ export default class Navigation extends Component {
       <div className="navigation-container" style={{ position: this.props.scrollingLock ? "fixed" : "absolute", top: this.props.scrollingLock ? "0" : null}}>
         <img className="nav-logo" src={logo}/>
         <a onClick={this.props.goToContactPage} className="contact-info" >
-          <h5>{this.props.contactOrTop ? "Contact Me" : "Back To Top"}</h5>
+          <h5>{this.props.contactOrTop ? "contact me" : "back to top"}</h5>
         </a>
       </div>
     )
