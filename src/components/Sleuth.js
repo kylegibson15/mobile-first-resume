0ab5e42eac6cd21a1daa8151a66a4bf9f91f013b
kylegibson15 <kylegibson15@gmail.com>
2018-02-23 17:30:23 -0700

    new component for the sleuth project

---
+++ src/components/Sleuth.js
@@ -0,0 +1,170 @@
+import React, {Component} from 'react';
+
+export default class Sleuth extends Component {
+
+  render() {
+    return (<div className="project sleuth-project">
+      <div className="fond">
+        <span className="s1-sleuth">Sleuth</span>
+        <span className="s2-sleuth">Sleuth</span>
+      </div>
+      <div className="card">
+
+        <div className="left-half">
+          <div className="thumbnail-logo">
+            <img className="proj-logo" src='https://d22lxpqe1nsxs.cloudfront.net/sleuth-logo-big.png'/>
+          </div>
+
+          <div className="tech-used">
+            <h4>TECHNOLOGIES USED</h4>
+
+            <div className="project-skills-v3">
+
+                <img className="proj-skill" src='https://d22lxpqe1nsxs.cloudfront.net/postgresql-logo.png' height="60" alt=""/>
+
+                <img className="proj-skill" src='https://d22lxpqe1nsxs.cloudfront.net/node.png' height="60" alt=""/>
+
+                <img className="proj-skill" src='https://d22lxpqe1nsxs.cloudfront.net/jsx.png' height="60" alt=""/>
+
+                <img className="proj-skill" src='https://d22lxpqe1nsxs.cloudfront.net/css3-logo.png' height="60" alt=""/>
+
+                <img className="proj-skill" src='https://d22lxpqe1nsxs.cloudfront.net/html5-no-border.png' height="60" alt=""/>
+
+                <img className="proj-skill" src='https://d22lxpqe1nsxs.cloudfront.net/knex.png' height="60" alt=""/>
+
+                <img className="proj-skill" src='https://d22lxpqe1nsxs.cloudfront.net/express.svg' height="60" alt=""/>
+
+                <img className="proj-skill" src='https://d22lxpqe1nsxs.cloudfront.net/google-chrome.png' height="60" alt=""/>
+
+                <img className="proj-skill" src='https://d22lxpqe1nsxs.cloudfront.net/reactjs2.png' height="60" alt=""/>
+
+                <img className="proj-skill" src='https://d22lxpqe1nsxs.cloudfront.net/ruuvi-tag-javascript.png' height="60" alt=""/>
+            </div>
+          </div>
+
+        </div>
+
+        <div className="right-half">
+          <div className="right">
+            <h1 className="h1">Sleuth</h1>
+
+            <div className="authors">
+              <div className="author">
+                <a className="thumbnail-anchor" href="https://github.com/kylegibson15" target="_blank">
+                  <img className="thumbnail-image" src="https://avatars2.githubusercontent.com/u/25071080?s=460&v=4"/>
+                  <h2 className="h2">Kyle G</h2>
+                </a>
+              </div>
+
+              <div className="author">
+                <a className="thumbnail-anchor" href="https://github.com/lukeromberg" target="_blank">
+                  <img className="thumbnail-image" src="https://avatars3.githubusercontent.com/u/30552918?s=460&v=4"/>
+                  <h2 className="h2">Luke R</h2>
+                </a>
+              </div>
+
+              <div className="author">
+                <a className="thumbnail-anchor" href="https://github.com/kalaver8" target="_blank">
+                  <img className="thumbnail-image" src="https://avatars1.githubusercontent.com/u/28827953?s=460&v=4"/>
+                  <h2 className="h2">Kelsey L</h2>
+                </a>
+              </div>
+
+              <div className="author">
+                <a className="thumbnail-anchor" href="https://github.com/davefrancese" target="_blank">
+                  <img className="thumbnail-image" src="https://avatars1.githubusercontent.com/u/1619671?s=460&v=4"/>
+                  <h2 className="h2">Dave F</h2>
+                </a>
+              </div>
+
+              <div className="author">
+                <a className="thumbnail-anchor" href="https://github.com/jordanreed23" target="_blank">
+                  <img className="thumbnail-image" src="https://avatars3.githubusercontent.com/u/26033242?s=460&v=4"/>
+                  <h2 className="h2">Jordan R</h2>
+                </a>
+              </div>
+
+              <div className="author">
+                <a className="thumbnail-anchor" href="https://github.com/iamjakerobbins" target="_blank">
+                  <img className="thumbnail-image" src="https://avatars0.githubusercontent.com/u/30421693?s=460&v=4"/>
+                  <h2 className="h2">Jake R</h2>
+                </a>
+              </div>
+
+              <div className="author">
+                <a className="thumbnail-anchor" href="https://github.com/tyslas" target="_blank">
+                  <img className="thumbnail-image" src="https://avatars3.githubusercontent.com/u/26233183?s=460&v=4"/>
+                  <h2 className="h2">Tito Y</h2>
+                </a>
+              </div>
+
+              <div className="author">
+                <a className="thumbnail-anchor" href="https://github.com/willofmike" target="_blank">
+                  <img className="thumbnail-image" src="https://avatars0.githubusercontent.com/u/28425471?s=460&v=4"/>
+                  <h2 className="h2">Mike W</h2>
+                </a>
+              </div>
+
+              <div className="author">
+                <a className="thumbnail-anchor" href="https://github.com/geralle" target="_blank">
+                  <img className="thumbnail-image" src="https://avatars1.githubusercontent.com/u/29743406?s=460&v=4"/>
+                  <h2 className="h2">Geralle</h2>
+                </a>
+              </div>
+
+            </div>
+
+
+            <div className="separator"></div>
+
+            <p>Sleuth is an app designed to create an even playing field for all ISPs. When using the app, you can check your download speed and compare top download speeds amongst other ISPs.</p>
+          </div>
+
+          <a className="fab-three sleu" href="https://speed-sleuth.firebaseapp.com/" target="_blank">
+
+            <div class="front-card">
+              <img className="github" src='https://d22lxpqe1nsxs.cloudfront.net/walter-white.png'/>
+            </div>
+
+            <div class="reverse-card">
+              <p>Click</p>
+              <p>4</p>
+              <p>Site</p>
+            </div>
+
+          </a>
+
+          <a className="fab-two sleu" href="https://chrome.google.com/webstore/detail/sleuth/ljcgfcpjeioeodhbpjjfamlikpgmnhno/related?hl=en" target="_blank">
+
+            <div class="front-card">
+              <img className="github" src='https://d22lxpqe1nsxs.cloudfront.net/google-chrome.png'/>
+            </div>
+
+            <div class="reverse-card">
+              <p>Click</p>
+              <p>2</p>
+              <p>Get</p>
+            </div>
+
+          </a>
+
+          <a className="fab sleu" href="https://github.com/g64-sleuth/Sleuth-Chrome" target="_blank">
+
+            <div class="front-card">
+              <img className="github" src='https://d22lxpqe1nsxs.cloudfront.net/white-github.png'/>
+            </div>
+
+            <div class="reverse-card">
+              <p>Click</p>
+              <p>4</p>
+              <p>Code</p>
+            </div>
+
+          </a>
+        </div>
+
+      </div>
+
+    </div>)
+  }
+}
