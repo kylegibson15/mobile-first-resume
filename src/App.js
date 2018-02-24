1ad0bdd9e3132b39665e744eb6f71d4e06b9511e
kylegibson15 <kylegibson15@gmail.com>
2018-02-23 20:47:16 -0700

    remove unnecessary code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import './App.css';
+
 import Main from './components/Main';
 import AboutMeV2 from './components/AboutMeV2';
 import ContactV2 from './components/ContactV2';
@@ -14,47 +15,29 @@ class App extends Component {
     super(props);
 
     this.state = {
-      text: ['contact me', 'call me', 'email me', 'say hello'],
-      count: 0,
       scrollingLock: false,
-      contactOrTop: true,
+      contactOrTop: true
     }
 
-    this.handleLoad = this.handleLoad.bind(this);
     this.handleScroll = this.handleScroll.bind(this)
     this.goToContactPage = this.goToContactPage.bind(this)
     this.handleContactText = this.handleContactText.bind(this)
-    // this.changeValue = this.changeValue.bind(this)
+    this.scrollDownComponent = this.scrollDownComponent.bind(this)
   }
 
   componentDidMount() {
     window.addEventListener('load', this.handleLoad);
     window.addEventListener('scroll', this.handleScroll);
-    window.addEventListener('scroll', this.handleContactText)
+    window.addEventListener('scroll', this.handleContactText);
+    window.addEventListener('scroll', this.scrollDownComponent);
 
   }
   componentWillUnmount() {
     window.removeEventListener('scroll', this.handleScroll);
   }
-  // async componentDidUpdate() {
-  //   window.setTimeout(function() {
-  //     this.changeValue();
-  //   }.bind(this), 3000)
-  // }
-  handleLoad() {
-    this.setState({lineClass: "hover", logoClass: "logo-hover"})
-    setTimeout(function() {
-      this.setState({bounceClass: "bounce"})
-    }.bind(this), 2000)
-  }
-
-  // changeValue() {
-  //   if(this.state.count < 4){
-  //     this.setState({count: this.state.count + 1});
-  //   }else{
-  //     this.setState({count: 0});
-  //   }
-  //   console.log('line 45', this.state.count);
+  // scrollDownComponent() {
+  //   if (window.scrollY) {}
+  //
   // }
   handleScroll() {
     let Y = window.innerHeight
@@ -66,9 +49,9 @@ class App extends Component {
   }
   handleContactText() {
     let contactY = window.innerHeight * 5 + (window.innerHeight / 2)
-    if(window.scrollY > contactY){
+    if (window.scrollY > contactY) {
       this.setState({contactOrTop: false})
-    }else{
+    } else {
       this.setState({contactOrTop: true})
     }
   }
@@ -76,26 +59,22 @@ class App extends Component {
     let contactY = window.innerHeight * 5
     let contact = document.getElementsByClassName("contactv2-container")[0]
     let top = document.getElementsByClassName("aboutmev2-container")[0]
-    if(window.scrollY > contactY){
+    if (window.scrollY > contactY) {
       top.scrollIntoView({behavior: 'smooth'})
-    }else{
+    } else {
       contact.scrollIntoView({behavior: 'smooth'})
     }
   }
 
   render() {
     return (<div className="App">
-      <Main />
+      <Main/>
 
-
-      <AboutMeV2 contactOrTop={this.state.contactOrTop} goToContactPage={this.goToContactPage}
-        scrollingLock={this.state.scrollingLock}/>
-
-      {/* <ProjectsV2 /> */}
-      <HealthAlarm />
-      <Sleuth />
-      <Slelp />
-      <Brunch />
+      <AboutMeV2 contactOrTop={this.state.contactOrTop} goToContactPage={this.goToContactPage} scrollingLock={this.state.scrollingLock}/> {/* <ProjectsV2 /> */}
+      <HealthAlarm/>
+      <Sleuth/>
+      <Slelp/>
+      <Brunch/>
 
       <ContactV2 text={this.state.text} changeValue={this.changeValue} count={this.state.count}/>
 
